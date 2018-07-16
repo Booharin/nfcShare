b777627cc48e89d1d39c35fc43bae81a453c983b
Booharin <booharin@bk.ru>
2018-07-16 15:30:16 +0300

    Added .gitignore

---
+++ nfcShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nfcShare
+//
+//  Created by Александр Бухарин on 16.07.2018.
+//  Copyright © 2018 umbrella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
